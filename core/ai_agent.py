041111a60ec91e9e7dc5784a995f05fddfc728fa
Abhijith JR <Achuabhijithjr@outlook.com>
2025-03-15 23:28:20 +0530

    convert to embeddings successfully

--- core/ai_agent.py
+++ core/ai_agent.py
@@ -1,7 +1,8 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings  # Corrected import
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from sentence_transformers import SentenceTransformer
 import os
 
 class AI:
@@ -25,25 +26,26 @@ class AI:
         print(f"from {len(self.documents)} to {len(self.chunks)}") #corrected chunk.
 
     def get_embeddings(self):
-        embedder = HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mini-L6-v2")
+        # embedder = HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mini-L6-v2")
+        embedder = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
         self.text_chunk = [chunk.page_content for chunk in self.chunks]
-        self.chunk_embeddings = embedder.embed_documents(self.text_chunk) #embed the chunks.
+        self.chunk_embeddings = embedder.encode(self.text_chunk) #embed the chunks.
         print("convert to embeddings successfully!")
         return embedder
     
     
     def embedding_to_DB(self):
         embedder=self.get_embeddings()
-        if not os.path.exists(self.presistant_directory):
-            os.mkdir(self.presistant_directory)
+        if not os.path.exists(self.persistent_directory):
+            os.mkdir(self.persistent_directory)
             
         self.db= FAISS.from_documents(self.chunks   ,embedder)
-        self.db.save_local(self.presistant_directory)
+        self.db.save_local(self.persistent_directory)
         print("db saved sucesfully!")
         
         
     def load_db(self):
-        self.db =FAISS.load_local(self.presistant_directory,HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mini-L6-v2"),allow_dangerous_deserialization=True)
+        self.db =FAISS.load_local(self.persistent_directory,HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-mini-L6-v2"),allow_dangerous_deserialization=True)
         print("vector_db_loaded_sucessfully")
     
         
