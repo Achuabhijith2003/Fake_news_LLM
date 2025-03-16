93467985cd5f26f42dd9f196380e720d377ec497
Abhijith JR <Achuabhijithjr@outlook.com>
2025-03-16 20:20:28 +0530

    llm responed correctlly

--- Config/config.py
+++ Config/config.py
@@ -1,6 +1,11 @@
 # Configurations 
 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+hugging_face_token=os.getenv("hugging_face_token")
+Gemini_key=os.getenv("Gemini_key")
 
 news="DB/News.txt"
--- core/ai_agent.py
+++ core/ai_agent.py
@@ -4,6 +4,10 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings #import the 
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from google.generativeai import generative_models
+from Config.config import Gemini_key
+from langchain.prompts.prompt import PromptTemplate
 
 
 class AI:
@@ -56,16 +60,40 @@ class AI:
     def Search_document(self):
         retrive=self.db.as_retriever(search_type="similarity",search_kwarg={"k":6})
         retrivered_doc=retrive.invoke(self.query)
-        print(f"Result from DB{retrivered_doc[0].page_content}")
+        # print(f"Result from DB{retrivered_doc[0].page_content}")
+        self.retrivered_doc_content=" ".join([doc.page_content for doc in retrivered_doc ])
+        
+        
+    # promt template
+    def Promt(self):
+        self.promt_template=PromptTemplate(template="Read this para carefully {retrivered_doc} and Question: {query} , responed with true or false ")
+        
+    # using llm
+    def model(self):
+        llm=ChatGoogleGenerativeAI(model="gemini-2.0-flash",verbose=True,temperature=0.1,api_key=Gemini_key)
+        promt= self.promt_template.format(
+            retrivered_doc=self.retrivered_doc_content,
+            query=self.query
+        )
+        respones=llm.invoke(input=promt)
+        print (respones)
+        
 
 path_data = "D:/KMCT/miniproject/fakdec/Datasets"
 path_db = "DB/faiss"
-query = "A>Basic or Central Economic Problems: "
+# query = "is Central Economic Problems in 5 th module "
 
-obj = AI(path_data, path_db, query)
-obj.load_documents()
-obj.Text_Splitter()
-obj.get_embeddings()
-obj.embedding_to_DB()
+obj = AI(path_data, path_db, query=None)
+# obj.load_documents()
+# obj.Text_Splitter()
+# obj.get_embeddings()
+# obj.embedding_to_DB()
 obj.load_db()
-obj.Search_document()
+# obj.Search_document()
+
+while True:
+    promt=input("enter the query: ")
+    obj.query=promt
+    obj.Search_document()
+    obj.Promt()
+    obj.model()
